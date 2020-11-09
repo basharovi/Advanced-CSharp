36a86c071d761317d72b8d6a1cbc9b979772a5da
Bashar Ovi <bashar.ovi@outlook.com>
2020-11-09 16:05:31 +0600

    Added Empty Project of Events

---
+++ Events/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
