7a5a61d24e1a5f87b743a694d452ad6c078cacaa
Bashar Ovi <bashar.ovi@outlook.com>
2020-11-09 16:57:57 +0600

    Added a Simple Example of Events

--- Events/Program.cs
+++ Events/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using Delegates.Math;
 
 namespace Events
 {
     class Program
     {
+        static event SimpleCalculator.Calculate AfterCalculation;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            AfterCalculation += Operator.Sum;
+            AfterCalculation += Operator.Sub;
+            AfterCalculation += Operator.Mul;
+
+            if (AfterCalculation == null) 
+                return;
+
+            var result = AfterCalculation(50, 25);
+
+            Console.WriteLine(result);
         }
     }
 }
