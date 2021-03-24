409a221eb1944ef6bd9609228e2ff67a64784a12
Bashar Ovi <mominur.ovi@savethechildren.org>
2021-03-24 14:25:42 +0600

    Added New Project of Record

---
+++ Records/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Records
+{
+    class Program
+    {
+        record Student(string FirstName, string LastName, int Age);
+
+        static void Main(string[] args)
+        {
+
+            var aStudent = new Student("Bashar", "Ovi", 23);
+
+            Console.WriteLine($"{aStudent.FirstName} \n" +
+                $"{aStudent.LastName} \n{aStudent.Age}");
+        }
+    }
+}
